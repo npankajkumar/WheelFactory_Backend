921c4400a22eda3f27c44323f3214ebbfce92bdb
npankajkumar <npankajkumar@actalentservices.com>
2024-09-16 21:06:37 +0000

    image upload post api for 2nd controller

--- TheWheelFactory/WheelFactory/Models/PaintDTO.cs
+++ TheWheelFactory/WheelFactory/Models/PaintDTO.cs
@@ -5,7 +5,7 @@
         public int OrderId {  get; set; }
         public string PType {  get; set; }
         public string PColor { get; set; }
-        public string ImageUrl { get; set; }
+        public IFormFile ImageUrl { get; set; }
         public string Notes { get; set; }
         public string Status { get; set; }
         public DateTime? CreatedAt { get; set; } = DateTime.UtcNow.AddHours(5).AddMinutes(30);
--- TheWheelFactory/WheelFactory/Models/SandDTO.cs
+++ TheWheelFactory/WheelFactory/Models/SandDTO.cs
@@ -6,7 +6,7 @@ namespace WheelFactory.Models
     {
         public int OrderId { get; set; }
         public string SandBlastingLevel { get; set; }
-        public string ImageUrl { get; set; }
+        public IFormFile ImageUrl { get; set; }
         public string Notes { get; set; }
         public string Status { get; set; }
         public DateTime? CreatedAt { get; set; } = DateTime.UtcNow.AddHours(5).AddMinutes(30);
